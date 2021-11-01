548c8672857550bf4fd6218933fb27a23d35885d
MarcoP1993 <perea_10@hotmail.com>
2021-11-01 03:00:53 +0100

    Version 0.5 en construccion

--- app/src/main/java/com/example/mysimracing/LoginActivity.java
+++ app/src/main/java/com/example/mysimracing/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,7 +21,14 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText edt_correo_inicio = null;
     private EditText edt_password_inicio = null;
+    private Button btn_acceder;
+
     private FirebaseAuth firebaseAuth;
+
+    //variables con los datos para inicar sesion
+    private String email = "";
+    private String password = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +40,26 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.Theme_AppCompat_DayNight_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        edt_correo_inicio = findViewById(R.id.correo_inicio);
-        edt_password_inicio = findViewById(R.id.password_inicio);
+        edt_correo_inicio = (EditText) findViewById(R.id.correo_inicio);
+        edt_password_inicio = (EditText) findViewById(R.id.password_inicio);
+        btn_acceder = (Button) findViewById(R.id.boton_acceso);
+
         firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_acceder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email = edt_correo_inicio.getText().toString();
+                password = edt_password_inicio.getText().toString();
+
+                if(!email.isEmpty() && !password.isEmpty()){
+                    acceso();
+
+                }else {
+                    Toast.makeText(LoginActivity.this, "Completa todos los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
@@ -43,9 +68,19 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void acceso(View view) {
-           Intent intent = new Intent(this,MainActivity.class);
-           startActivity(intent);
+    public void acceso() {
+           firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+                    }else {
+                        Toast.makeText(LoginActivity.this, "Acceso denegado, correo o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                    }  
+               }
+           });
     }
 
     public void RecuperarClave(View view) {
@@ -53,4 +88,13 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /*@Override
+    protected void onStart() {
+        super.onStart();
+
+        if(firebaseAuth.getCurrentUser() !=null){
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }*/
 }
--- app/src/main/java/com/example/mysimracing/MainActivity.java
+++ app/src/main/java/com/example/mysimracing/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.mysimracing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView nombre_usuario;
+    private TextView nickname_usuario;
+
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre_usuario = (TextView) findViewById(R.id.txt_nombre_usuario);
+        nickname_usuario = (TextView) findViewById(R.id.txt_nickname_usuario);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        recuperarInfoUsusario();
+
+    }
+
+    public void recuperarInfoUsusario(){
+        String id = firebaseAuth.getCurrentUser().getUid();
+
+        databaseReference.child("Usuarios").child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    String nombre = snapshot.child("Nombre").getValue().toString();
+                    String nickname = snapshot.child("Nickname").getValue().toString();
+
+                    nombre_usuario.setText(nombre);
+                    nickname_usuario.setText(nickname);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
